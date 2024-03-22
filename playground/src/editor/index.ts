2b3db274a94104dddf1482d933ea22a5379e2c8e
yijie4188 <yijie4188@gmail.com>
2024-03-23 04:11:29 +0800

    feat: support `onHoverElement` for plugin

--- playground/src/editor/index.ts
+++ playground/src/editor/index.ts
@@ -1,11 +1,25 @@
 import './index.scss'
 
 import { type BundledLanguage, type BundledTheme, getHighlighter } from 'shiki'
+import { throttle } from '../utils'
+
+export interface TextRange {
+  offset: number
+  line: number
+  start: number
+  end: number
+}
+export interface OnHoverElementContext {
+  content: string
+  element: Element
+  raw: string
+}
 
 export interface Plugin {
-  name: string
-  install: (editor: Shikitor) => void
+  name?: string
+  install?: (editor: Shikitor) => void
   onDispose?: () => void
+  onHoverElement?: (range: TextRange, context: OnHoverElementContext) => void
 }
 
 export interface ShikitorOptions {
@@ -44,7 +58,7 @@ export function create(target: HTMLDivElement, options: ShikitorOptions): Shikit
     readOnly,
     lineNumbers = 'on',
     value, onChange,
-    plugins
+    plugins = []
   } = options
 
   const [input, output] = initInputAndOutput(options)
@@ -98,6 +112,56 @@ export function create(target: HTMLDivElement, options: ShikitorOptions): Shikit
     render()
   }
   input.addEventListener('input', () => changeValue(input.value))
+  let prevOutputHoverElement: Element | null = null;
+  input.addEventListener("mousemove", throttle(e => {
+    input.style.pointerEvents = "none";
+    output.style.pointerEvents = "auto";
+    const outputHoverElement = document.elementFromPoint(e.clientX, e.clientY);
+    input.style.pointerEvents = "";
+    output.style.pointerEvents = "";
+    if (outputHoverElement === prevOutputHoverElement) {
+      return;
+    }
+    prevOutputHoverElement = outputHoverElement;
+    if (outputHoverElement === null) {
+      return;
+    }
+    if (
+      outputHoverElement.className.includes("shiki-editor")
+      && outputHoverElement.className.includes("output")
+    ) {
+      return;
+    }
+
+    if (!outputHoverElement?.className.includes("position")) {
+      return;
+    }
+
+    const offsetStr = /offset:(\d+)/
+      .exec(outputHoverElement.className)
+      ?.[1];
+    if (!offsetStr) {
+      return;
+    }
+    const offset = Number(offsetStr);
+    if (isNaN(offset)) {
+      return;
+    }
+    const [line, start, end] = /position:(\d+):(\d+),(\d+)/
+      .exec(outputHoverElement.className)
+      ?.slice(1)
+      ?.map(Number)
+    ?? [];
+    if (!line || !start || !end || [line, start, end].some(isNaN)) {
+      return;
+    }
+
+    plugins.map(plugin => plugin.onHoverElement?.({ offset, line: line, start: start, end: end }, {
+      content: input.value.slice(start - 1, end - 1),
+      element: outputHoverElement,
+      raw: input.value,
+    }));
+  }, 50));
 
   render()
   const shikitor: Shikitor = {
@@ -108,6 +172,6 @@ export function create(target: HTMLDivElement, options: ShikitorOptions): Shikit
       changeValue(value)
     }
   }
-  plugins?.map(plugin => plugin.install(shikitor))
+  plugins.map(plugin => plugin.install?.(shikitor))
   return shikitor
 }
