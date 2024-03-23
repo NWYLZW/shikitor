5ff469c5a66924a34b08bc03c5c1df2c4ada4c4a
yijie4188 <yijie4188@gmail.com>
2024-03-23 23:23:20 +0800

    feat: add listen util function

--- playground/src/utils.ts
+++ playground/src/utils.ts
@@ -147,3 +147,22 @@ export function decorateTokens(
   }
   return result
 }
+
+interface EventEmitter {
+  addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void
+  removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void
+}
+export function listen<K extends keyof DocumentEventMap>(
+  element: Document, type: K, listener: (this: Document, ev: DocumentEventMap[K]) => any, options?: boolean | AddEventListenerOptions
+): () => void
+export function listen<
+  Elem extends HTMLElement, EventMap extends HTMLElementEventMap, K extends keyof EventMap
+>(
+  element: Elem, type: K, listener: (this: Elem, ev: EventMap[K]) => any, options?: boolean | AddEventListenerOptions
+): () => void
+export function listen(
+  element: EventEmitter, type: string, listener: (ev: Event) => any, options?: boolean | AddEventListenerOptions
+): () => void {
+  element.addEventListener(type, listener, options)
+  return () => element.removeEventListener(type, listener, options)
+}
