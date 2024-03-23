ca3a1eefc6b23ea17d316ccf11f1b98e63a57901
yijie4188 <yijie4188@gmail.com>
2024-03-23 17:34:29 +0800

    refactor: split tokens render

--- playground/src/utils.ts
+++ playground/src/utils.ts
@@ -47,6 +47,14 @@ export function getRawTextHelper(text: string) {
 
 export type DecoratedThemedToken = ThemedToken & { tagName?: string }
 export type Decoration = Pick<DecorationItem, 'start' | 'end' | 'tagName'>
+export type ResolvedDecoration =
+  & Omit<
+    Decoration, 'start' | 'end'
+  >
+  & {
+    start: ResolvedPosition
+    end: ResolvedPosition
+  }
 
 export function decorateTokens(
   rawText: string,
@@ -54,18 +62,7 @@ export function decorateTokens(
   decorations: Decoration[]
 ) {
   const { getResolvedPositions } = getRawTextHelper(rawText)
-  const tokenDecorationsMap = new Map<
-    DecoratedThemedToken,
-    (
-      & Omit<
-        Decoration, 'start' | 'end'
-      >
-      & {
-        start: ResolvedPosition
-        end: ResolvedPosition
-      }
-    )[]
-  >()
+  const tokenDecorationsMap = new Map<DecoratedThemedToken, ResolvedDecoration[]>()
   for (const decoration of decorations) {
     const { start, end, ...omitStartAndEndDecoration } = decoration
     const startResolved = getResolvedPositions(start)
@@ -107,34 +104,39 @@ export function decorateTokens(
       const tokenEnd = token.offset + token.content.length
       const decorations = tokenDecorationsMap.get(token)
       if (decorations) {
+        const points: Record<number, ResolvedDecoration[]> = {}
+        points[0] = []
+        points[token.content.length] = []
         for (const decoration of decorations) {
-          const { start, end, tagName } = decoration
+          const { start, end } = decoration
           const l = start.offset > tokenStart
             ? start.offset - tokenStart
             : 0
           const r = end.offset < tokenEnd
             ? end.offset - tokenStart
             : token.content.length
-          const content = r === token.content.length
-            ? token.content.slice(l)
-            : token.content.slice(l, r)
-          if (l !== 0) {
+          points[l] = points[l] ?? []
+          points[l].push(decoration)
+          points[r] = points[r] ?? []
+        }
+        const pointIndexes = Object.keys(points).map(Number).sort((a, b) => a - b)
+        for (let i = 0; i < pointIndexes.length - 1; i++) {
+          const l = pointIndexes[i]
+          const r = pointIndexes[i + 1]
+          const lDecorations = points[l]
+          if (lDecorations.length) {
+            const tagNames = lDecorations.map(d => d.tagName).filter(Boolean)
             ntks.push({
               ...token,
-              content: token.content.slice(0, l)
+              content: token.content.slice(l, r),
+              offset: token.offset + l,
+              tagName: tagNames.join(' ')
             })
-          }
-          ntks.push({
-            ...token,
-            content,
-            offset: token.offset + l,
-            tagName
-          })
-          if (r !== token.content.length) {
+          } else {
             ntks.push({
               ...token,
-              content: token.content.slice(r),
-              offset: token.offset + r
+              content: token.content.slice(l, r),
+              offset: token.offset + l
             })
           }
         }
