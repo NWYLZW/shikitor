e97859a2608274ca0c6da1ad55498715243b079f
yijie4188 <yijie4188@gmail.com>
2024-03-24 00:16:03 +0800

    feat: abstract `ListenOptions`

--- playground/src/utils.ts
+++ playground/src/utils.ts
@@ -152,16 +152,18 @@ interface EventEmitter {
   addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void
   removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void
 }
+interface ListenOptions extends AddEventListenerOptions {
+}
 export function listen<K extends keyof DocumentEventMap>(
-  element: Document, type: K, listener: (this: Document, ev: DocumentEventMap[K]) => any, options?: boolean | AddEventListenerOptions
+  element: Document, type: K, listener: (this: Document, ev: DocumentEventMap[K]) => any, options?: boolean | ListenOptions
 ): () => void
 export function listen<
   Elem extends HTMLElement, EventMap extends HTMLElementEventMap, K extends keyof EventMap
 >(
-  element: Elem, type: K, listener: (this: Elem, ev: EventMap[K]) => any, options?: boolean | AddEventListenerOptions
+  element: Elem, type: K, listener: (this: Elem, ev: EventMap[K]) => any, options?: boolean | ListenOptions
 ): () => void
 export function listen(
-  element: EventEmitter, type: string, listener: (ev: Event) => any, options?: boolean | AddEventListenerOptions
+  element: EventEmitter, type: string, listener: (ev: Event) => any, options?: boolean | ListenOptions
 ): () => void {
   element.addEventListener(type, listener, options)
   return () => element.removeEventListener(type, listener, options)
