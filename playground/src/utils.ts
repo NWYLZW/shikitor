7d173c826fec7200651f52105269221eafa3e7d7
yijie4188 <yijie4188@gmail.com>
2024-03-23 16:31:34 +0800

    refactor: split to decorating tokens

--- playground/src/utils.ts
+++ playground/src/utils.ts
@@ -1,4 +1,4 @@
-import type { DecorationItem, ThemedToken } from '@shikijs/core'
+import type { DecorationItem, OffsetOrPosition, ResolvedPosition, ThemedToken } from '@shikijs/core'
 
 export function throttle<T extends (...args: any[]) => void>(fn: T, delay: number) {
   let last = 0
@@ -12,68 +12,71 @@ export function throttle<T extends (...args: any[]) => void>(fn: T, delay: numbe
 }
 
 export function getRawTextHelper(text: string) {
-  return {
-    getOffset: (line: number, character: number) => {
-      const lines = text.split('\n')
-      return lines.slice(0, line - 1).reduce((acc, line) => acc + line.length + 1, 0) + character
-    },
-    getPosition: (offset: number) => {
-      const lines = text.split('\n')
-      let line = 1
-      let character = 1
-      for (const lineContent of lines) {
-        if (offset <= lineContent.length) {
-          character = offset
-          break
-        }
-        offset -= lineContent.length + 1
-        line++
+  function getOffset(line: number, character: number) {
+    const lines = text.split('\n')
+    return lines.slice(0, line - 1).reduce((acc, line) => acc + line.length + 1, 0) + character
+  }
+  function getPosition(offset: number) {
+    const lines = text.split('\n')
+    let line = 1
+    let character = 1
+    for (const lineContent of lines) {
+      if (offset <= lineContent.length) {
+        character = offset
+        break
       }
-      return { line, character }
+      offset -= lineContent.length + 1
+      line++
     }
+    return { line, character }
+  }
+  function getResolvedPositions(oop: OffsetOrPosition) {
+    return {
+      offset: typeof oop === 'number'
+        ? oop
+        : getOffset(oop.line, oop.character),
+      ...(typeof oop === 'number' ? getPosition(oop) : oop)
+    }
+  }
+  return {
+    getOffset,
+    getPosition,
+    getResolvedPositions
   }
 }
 
 export type DecoratedThemedToken = ThemedToken & { tagName?: string }
+export type Decoration = Pick<DecorationItem, 'start' | 'end' | 'tagName'>
 
 export function decorateTokens(
   rawText: string,
   tokensLines: DecoratedThemedToken[][],
-  decorations: Pick<DecorationItem, 'start' | 'end' | 'tagName'>[]
+  decorations: Decoration[]
 ) {
-  const { getOffset, getPosition } = getRawTextHelper(rawText)
-  const result: DecoratedThemedToken[][] = []
-  for (const { start, end, tagName } of decorations) {
-    const startOffset = typeof start === 'number'
-      ? start
-      : getOffset(start.line, start.character)
-    const endOffset = typeof end === 'number'
-      ? end
-      : getOffset(end.line, end.character)
-    const startPosition = typeof start === 'number'
-      ? getPosition(start)
-      : start
-    const endPosition = typeof end === 'number'
-      ? getPosition(end)
-      : end
-    const tokens = tokensLines[startPosition.line - 1]
+  const { getResolvedPositions } = getRawTextHelper(rawText)
+  const tokenDecorationsMap = new Map<
+    DecoratedThemedToken,
+    (
+      & Omit<
+        Decoration, 'start' | 'end'
+      >
+      & {
+        start: ResolvedPosition
+        end: ResolvedPosition
+      }
+    )[]
+  >()
+  for (const decoration of decorations) {
+    const { start, end, ...omitStartAndEndDecoration } = decoration
+    const startResolved = getResolvedPositions(start)
+    const endResolved = getResolvedPositions(end)
+    const tokens = tokensLines[startResolved.line - 1]
     if (!tokens) {
       continue
     }
-    const newTokensLine: typeof tokens = []
     for (const token of tokens) {
       const tokenStart = token.offset
       const tokenEnd = token.offset + token.content.length
-      // --123--
-      // ^^
-      if (endOffset <= tokenStart) {
-        newTokensLine.push(token)
-      }
-      // --123--
-      //      ^^
-      if (tokenEnd <= startOffset) {
-        newTokensLine.push(token)
-      }
       // --123--
       //  ^^^
       // --123--
@@ -84,38 +87,61 @@ export function decorateTokens(
       //   ^^^^
       // --123--
       //    ^^^
-      const l = startOffset > tokenStart
-        ? startOffset - tokenStart
-        : 0
-      const r = endOffset < tokenEnd
-        ? endOffset - tokenStart
-        : token.content.length
-      if (l < r) {
-        const content = r === token.content.length
-          ? token.content.slice(l)
-          : token.content.slice(l, r)
-        if (l !== 0) {
-          newTokensLine.push({
-            ...token,
-            content: token.content.slice(0, l)
-          })
-        }
-        newTokensLine.push({
-          ...token,
-          content,
-          offset: token.offset + l,
-          tagName
+      if (!(endResolved.offset <= tokenStart || tokenEnd <= startResolved.offset)) {
+        const tokenDecorations = tokenDecorationsMap.get(token) ?? []
+        tokenDecorationsMap.set(token, tokenDecorations)
+        tokenDecorations.push({
+          ...omitStartAndEndDecoration,
+          start: startResolved,
+          end: endResolved
         })
-        if (r !== token.content.length) {
-          newTokensLine.push({
-            ...token,
-            content: token.content.slice(r),
-            offset: token.offset + r
-          })
-        }
       }
     }
-    result[startPosition.line - 1] = newTokensLine
+  }
+  const result: DecoratedThemedToken[][] = []
+  for (const tokens of tokensLines) {
+    const ntks: DecoratedThemedToken[] = []
+    result.push(ntks)
+    for (const token of tokens) {
+      const tokenStart = token.offset
+      const tokenEnd = token.offset + token.content.length
+      const decorations = tokenDecorationsMap.get(token)
+      if (decorations) {
+        for (const decoration of decorations) {
+          const { start, end, tagName } = decoration
+          const l = start.offset > tokenStart
+            ? start.offset - tokenStart
+            : 0
+          const r = end.offset < tokenEnd
+            ? end.offset - tokenStart
+            : token.content.length
+          const content = r === token.content.length
+            ? token.content.slice(l)
+            : token.content.slice(l, r)
+          if (l !== 0) {
+            ntks.push({
+              ...token,
+              content: token.content.slice(0, l)
+            })
+          }
+          ntks.push({
+            ...token,
+            content,
+            offset: token.offset + l,
+            tagName
+          })
+          if (r !== token.content.length) {
+            ntks.push({
+              ...token,
+              content: token.content.slice(r),
+              offset: token.offset + r
+            })
+          }
+        }
+      } else {
+        ntks.push(token)
+      }
+    }
   }
   return result
 }
