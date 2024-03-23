1fab5ebd2399db642c8539758fa0ef95600c01aa
yijie4188 <yijie4188@gmail.com>
2024-03-23 22:16:55 +0800

    feat: add auto focus

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -7,11 +7,16 @@ const container = document.querySelector<HTMLDivElement>('#container')!
 
 console.log('Creating Shikitor instance')
 const shikitor = create(container, config)
+function init() {
+  shikitor.focus()
+}
+init()
 
 if (import.meta.hot) {
   import.meta.hot.accept('./config.ts', newModule => {
     if (!newModule) return
     const { default: newConfig } = newModule as unknown as { default: typeof config }
     shikitor.options = newConfig
+    init()
   })
 }
