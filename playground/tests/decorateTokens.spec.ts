36e869e5e8519b9db29e056df661c6c48f9450be
yijie4188 <yijie4188@gmail.com>
2024-03-24 23:26:49 +0800

    fix: eslint error

--- playground/tests/decorateTokens.spec.ts
+++ playground/tests/decorateTokens.spec.ts
@@ -1,35 +1,35 @@
 import { describe, expect, test } from 'vitest'
 
-import { decorateTokens } from '../src/utils'
+import { decorateTokens } from '../src/utils/decorateTokens'
 
 describe('decorateTokens', () => {
   test('single token and single decoration', () => {
     const tksLines = [
-      [{ "content": "123", "offset": 0, "color": "#E1E4E8", "fontStyle": 0 }]
+      [{ 'content': '123', 'offset': 0, 'color': '#E1E4E8', 'fontStyle': 0 }]
     ]
     expect(decorateTokens('123', tksLines, [
       { start: 0, end: 1, tagName: 'strong' }
     ]))
       .toStrictEqual([
         [
-          { "content": "1", "offset": 0, "color": "#E1E4E8", "fontStyle": 0, "tagName": "strong" },
-          { "content": "23", "offset": 1, "color": "#E1E4E8", "fontStyle": 0 }
+          { 'content': '1', 'offset': 0, 'color': '#E1E4E8', 'fontStyle': 0, 'tagName': 'strong' },
+          { 'content': '23', 'offset': 1, 'color': '#E1E4E8', 'fontStyle': 0 }
         ]
       ])
     expect(decorateTokens('123', tksLines, [
       { start: 0, end: 0, tagName: 'strong' }
     ]))
       .toStrictEqual([
-        [{ "content": "123", "offset": 0, "color": "#E1E4E8", "fontStyle": 0 }]
+        [{ 'content': '123', 'offset': 0, 'color': '#E1E4E8', 'fontStyle': 0 }]
       ])
     expect(decorateTokens('123', tksLines, [
       { start: 1, end: 2, tagName: 'strong' }
     ]))
       .toStrictEqual([
         [
-          { "content": "1", "offset": 0, "color": "#E1E4E8", "fontStyle": 0 },
-          { "content": "2", "offset": 1, "color": "#E1E4E8", "fontStyle": 0, "tagName": "strong" },
-          { "content": "3", "offset": 2, "color": "#E1E4E8", "fontStyle": 0 }
+          { 'content': '1', 'offset': 0, 'color': '#E1E4E8', 'fontStyle': 0 },
+          { 'content': '2', 'offset': 1, 'color': '#E1E4E8', 'fontStyle': 0, 'tagName': 'strong' },
+          { 'content': '3', 'offset': 2, 'color': '#E1E4E8', 'fontStyle': 0 }
         ]
       ])
     expect(decorateTokens('123', tksLines, [
@@ -37,13 +37,13 @@ describe('decorateTokens', () => {
     ]))
       .toStrictEqual([
         [
-          { "content": "123", "offset": 0, "color": "#E1E4E8", "fontStyle": 0, "tagName": "strong" }
+          { 'content': '123', 'offset': 0, 'color': '#E1E4E8', 'fontStyle': 0, 'tagName': 'strong' }
         ]
       ])
   })
   test('single token and multiple decorations', () => {
     const tksLines = [
-      [{ "content": "123", "offset": 0, "color": "#E1E4E8", "fontStyle": 0 }]
+      [{ 'content': '123', 'offset': 0, 'color': '#E1E4E8', 'fontStyle': 0 }]
     ]
     expect(decorateTokens('123', tksLines, [
       { start: 0, end: 1, tagName: 'strong' },
@@ -51,17 +51,17 @@ describe('decorateTokens', () => {
     ]))
       .toStrictEqual([
         [
-          { "content": "1", "offset": 0, "color": "#E1E4E8", "fontStyle": 0, "tagName": "strong" },
-          { "content": "2", "offset": 1, "color": "#E1E4E8", "fontStyle": 0, "tagName": "em" },
-          { "content": "3", "offset": 2, "color": "#E1E4E8", "fontStyle": 0 }
+          { 'content': '1', 'offset': 0, 'color': '#E1E4E8', 'fontStyle': 0, 'tagName': 'strong' },
+          { 'content': '2', 'offset': 1, 'color': '#E1E4E8', 'fontStyle': 0, 'tagName': 'em' },
+          { 'content': '3', 'offset': 2, 'color': '#E1E4E8', 'fontStyle': 0 }
         ]
       ])
   })
   test('multiple tokens and multiple decoration', () => {
     const tksLines = [
       [
-        { "content": "123", "offset": 0, "color": "#E1E4E8", "fontStyle": 0 },
-        { "content": "456", "offset": 3, "color": "#E1E4E8", "fontStyle": 0 }
+        { 'content': '123', 'offset': 0, 'color': '#E1E4E8', 'fontStyle': 0 },
+        { 'content': '456', 'offset': 3, 'color': '#E1E4E8', 'fontStyle': 0 }
       ]
     ]
     expect(decorateTokens('123456', tksLines, [
@@ -70,10 +70,10 @@ describe('decorateTokens', () => {
     ]))
       .toStrictEqual([
         [
-          { "content": "1", "offset": 0, "color": "#E1E4E8", "fontStyle": 0, "tagName": "strong" },
-          { "content": "23", "offset": 1, "color": "#E1E4E8", "fontStyle": 0 },
-          { "content": "4", "offset": 3, "color": "#E1E4E8", "fontStyle": 0, "tagName": "em" },
-          { "content": "56", "offset": 4, "color": "#E1E4E8", "fontStyle": 0 }
+          { 'content': '1', 'offset': 0, 'color': '#E1E4E8', 'fontStyle': 0, 'tagName': 'strong' },
+          { 'content': '23', 'offset': 1, 'color': '#E1E4E8', 'fontStyle': 0 },
+          { 'content': '4', 'offset': 3, 'color': '#E1E4E8', 'fontStyle': 0, 'tagName': 'em' },
+          { 'content': '56', 'offset': 4, 'color': '#E1E4E8', 'fontStyle': 0 }
         ]
       ])
   })
