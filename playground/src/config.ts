d13301e6d2e0505855cd111841ec0bcc96733325
yijie4188 <yijie4188@gmail.com>
2024-03-24 21:49:05 +0800

    feat: support load code from hash

--- playground/src/config.ts
+++ playground/src/config.ts
@@ -3,25 +3,52 @@ import type { ResolvedPosition } from '@shikijs/core'
 import type { ShikitorOptions } from './core/editor'
 import bracketMatcher from './plugins/bracket-matcher'
 import codeStyler from './plugins/code-styler'
+import { unzipStr, zipStr } from './utils'
 
-let defaultCode = `
+const DEFAULT_CODE = `
 console.log("Hello, World!")
 
 function add(a, b) {
   return a + b
 }
 `.trimStart()
+
+let code = DEFAULT_CODE
 let cursor: undefined | ResolvedPosition
+
+const [type, content] = location.hash.slice(1).split('/')
+if (content === undefined) {
+  // undefined behavior, reset hash to empty
+  // and don't jump page
+  location.hash = ''
+}
+switch (type) {
+  case 'zip-code':
+    try {
+      code = unzipStr(content)
+    } catch (e) {
+      console.error(e)
+    }
+    break
+  case 'code':
+    code = decodeURIComponent(content)
+    break
+  case 'base64':
+    code = atob(content)
+    break
+  case 'gist':
+    break
+}
 
 export default {
   get value() {
-    return defaultCode
+    return code
   },
   set value(value) {
-    defaultCode = value
+    code = value
   },
   onChange(value) {
-    defaultCode = value
+    code = value
   },
   get cursor() {
     return cursor
@@ -33,6 +60,20 @@ export default {
   theme: 'github-dark',
   plugins: [
     bracketMatcher,
-    codeStyler
+    codeStyler,
+    {
+      name: 'shikitor-saver',
+      onKeydown(e) {
+        if (e.key === 's' && (e.ctrlKey || e.metaKey)) {
+          e.preventDefault()
+          if (code === DEFAULT_CODE) return
+
+          const zipCode = zipStr(code)
+          const url = new URL(location.href)
+          url.hash = `zip-code/${zipCode}`
+          history.pushState(null, '', url.toString())
+        }
+      }
+    }
   ]
 } as ShikitorOptions
