6c1eb51d678e0a756bc86cfd87e750e7c32cced8
yijie4188 <yijie4188@gmail.com>
2024-03-24 14:05:43 +0800

    chore: replace esbuild by swc

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,6 +1,9 @@
 import * as process from 'process'
+import swc from 'rollup-plugin-swc3'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.BASE ?? '/'
+  base: process.env.BASE ?? '/',
+  esbuild: false,
+  plugins: [swc()]
 })
