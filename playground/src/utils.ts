d7406d2f9371fe9762a99454e5d13f0a4dd1a97d
yijie4188 <yijie4188@gmail.com>
2024-03-24 21:48:50 +0800

    feat: add special zip and unzip algorithm util function

--- playground/src/utils.ts
+++ playground/src/utils.ts
@@ -1,4 +1,5 @@
 import type { DecorationItem, OffsetOrPosition, ResolvedPosition, ThemedToken } from '@shikijs/core'
+import * as lz from 'lz-string'
 
 export function throttle<T extends (...args: any[]) => void>(fn: T, delay: number) {
   let last = 0
@@ -194,4 +195,22 @@ export function trimIndent(text: string) {
   const firstLine = lines[0]
   const indent = firstLine.match(/^\s*/)![0]
   return lines.map(line => line.replace(new RegExp(`^${indent}`), '')).join('\n')
+}
+
+const tag = 'shikitor-zip-tag'
+export function zipStr(str: string) {
+  return lz.compressToEncodedURIComponent(`${tag}:${lz.compressToEncodedURIComponent(str)}`)
+}
+export function unzipStr(str: string) {
+  const firstLevel = lz.decompressFromEncodedURIComponent(str)
+  if (!firstLevel.startsWith(`${tag}:`)) {
+    throw new Error('Invalid tag, content is not compressed')
+  }
+  const secondLevel = lz.decompressFromEncodedURIComponent(
+    firstLevel.slice(tag.length + 1)
+  )
+  if (secondLevel === null) {
+    throw new Error('Failed to decompress')
+  }
+  return secondLevel
 }
