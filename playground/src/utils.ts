3e13dca29ac33b627bf2b24950d1e7f4310dafe7
yijie4188 <yijie4188@gmail.com>
2024-03-24 22:48:53 +0800

    refactor: abstract listen

--- playground/src/utils.ts
+++ playground/src/utils.ts
@@ -1,24 +1,3 @@
-interface EventEmitter {
-  addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void
-  removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void
-}
-interface ListenOptions extends AddEventListenerOptions {
-}
-export function listen<K extends keyof DocumentEventMap>(
-  element: Document, type: K, listener: (this: Document, ev: DocumentEventMap[K]) => any, options?: boolean | ListenOptions
-): () => void
-export function listen<
-  Elem extends HTMLElement, EventMap extends HTMLElementEventMap, K extends keyof EventMap
->(
-  element: Elem, type: K, listener: (this: Elem, ev: EventMap[K]) => any, options?: boolean | ListenOptions
-): () => void
-export function listen(
-  element: EventEmitter, type: string, listener: (ev: Event) => any, options?: boolean | ListenOptions
-): () => void {
-  element.addEventListener(type, listener, options)
-  return () => element.removeEventListener(type, listener, options)
-}
-
 const memoized = new WeakMap<(...args: any[]) => unknown, [arguments: any[], returnType: unknown]>()
 export function lazy<Args extends unknown[], RT>(func: (...args: Args) => RT): (...args: Args) => RT {
   return (...args) => {
