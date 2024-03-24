4b1ece52450d216bfe2311539d874f89cb4fb249
yijie4188 <yijie4188@gmail.com>
2024-03-25 03:00:46 +0800

    chore: import vite-plugin-cdn2 to shaking shiki/wasm as external by cdn

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,8 +1,19 @@
 import swc from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { cdn } from 'vite-plugin-cdn2'
+import { unpkg } from 'vite-plugin-cdn2/resolver/unpkg'
 
 export default defineConfig({
   base: process.env.BASE ?? '/',
   esbuild: false,
-  plugins: [swc()]
+  plugins: [
+    swc(),
+    cdn({
+      resolve: unpkg(),
+      modules:[
+        // use shiki wasm cdn
+        // { name: 'shiki', global: 'Shiki', spare: 'dist/index.umd.js' }
+      ]
+    })
+  ]
 })
