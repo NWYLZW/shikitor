1507814f9c01b2fadbea2328aac86c43e9d43763
yijie4188 <yijie4188@gmail.com>
2024-03-25 14:24:46 +0800

    feat: add `importmap-generator` plugin, but the shiki dependency is not provided `package.json`
    shikijs/shiki#639

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,5 +1,5 @@
 import swc from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, type Plugin } from 'vite'
 
 export default defineConfig({
   base: process.env.BASE ?? '/',
@@ -10,6 +10,21 @@ export default defineConfig({
     }
   },
   plugins: [
+    importmapGenerator(),
     swc()
   ]
 })
+
+function importmapGenerator(): Plugin {
+  return {
+    name: 'importmap-generator',
+    configResolved(config) {
+      const { external = [] } = config.build.rollupOptions
+      for (const packageName of external as string[]) {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const packageJson = require(`${packageName}/package.json`)
+        console.log(packageJson)
+      }
+    }
+  }
+}
