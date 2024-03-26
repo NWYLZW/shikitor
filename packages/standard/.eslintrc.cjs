9aa59b2891fa35310f76ad41a6763ff3573d7bec
yijie4188 <yijie4188@gmail.com>
2024-03-26 21:49:47 +0800

    chore(standard): add unused imports eslint rule

--- packages/standard/.eslintrc.cjs
+++ packages/standard/.eslintrc.cjs
@@ -98,9 +98,19 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort', 'unused-imports'],
   rules: {
     'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        'vars': 'all',
+        'varsIgnorePattern': '^_',
+        'args': 'after-used',
+        'argsIgnorePattern': '^_',
+      },
+    ],
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     'semi': ['error', 'never'],
