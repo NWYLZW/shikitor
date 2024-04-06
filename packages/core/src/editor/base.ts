b361877fd23d844f14727cc1b8174859ad978b9c
yijie4188 <yijie4188@gmail.com>
2024-04-07 00:16:02 +0800

    feat(core): add ProviderResult type-level util function

--- packages/core/src/editor/base.ts
+++ packages/core/src/editor/base.ts
@@ -2,6 +2,7 @@ import type { OffsetOrPosition, ResolvedPosition } from '@shikijs/core'
 import type { BundledLanguage } from 'shiki'
 
 import type { ResolvedTextRange, TextRange } from '../base'
+import type { Awaitable, Nullable } from '../types'
 
 export type Cursor = OffsetOrPosition
 export type Selection = TextRange
@@ -13,3 +14,5 @@ export type LanguageSelector = '*' | BundledLanguage | readonly BundledLanguage[
 export interface IDisposable {
   dispose(): void
 }
+
+export type ProviderResult<T> = Awaitable<Nullable<T>>
