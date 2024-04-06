f9bcb108a8555adec2305c76c5b2fda7c8c764b2
yijie4188 <yijie4188@gmail.com>
2024-04-07 00:15:33 +0800

    fix(core): make Awaitable PromiseLike not only Promise

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -5,7 +5,7 @@ export type PickByValue<T, ValueType> = Pick<T, {
     [Key in keyof T]: [ValueType] extends [T[Key]] ? Key : never
 }[keyof T]>
 
-export type Awaitable<T> = Awaited<T> extends infer U ? U | Promise<U> : never
+export type Awaitable<T> = Awaited<T> extends infer U ? U | PromiseLike<U> : never
 
 export type RecursiveReadonly<T> = T extends Record<string, any>
   ? { readonly [K in keyof T]: RecursiveReadonly<T[K]> }
