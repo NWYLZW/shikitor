9c3d9101d48531dc43df3155be8714408851b097
yijie4188 <yijie4188@gmail.com>
2024-04-07 00:17:21 +0800

    feat(core): add expression quick completions plugin

---
+++ packages/core/src/plugins/expression-quick-completions.ts
@@ -0,0 +1,33 @@
+import { definePlugin } from '../plugin'
+
+export default definePlugin({
+  name: 'expression-quick-completions',
+  install() {
+    const dependDisposable = this.depend(['provide-completions'], shikitor => shikitor.registerCompletionItemProvider('*', {
+      triggerCharacters: ['.'],
+      provideCompletionItems({ value }, position) {
+        const prevChar = value.charAt(position.offset - 2)
+        const editorHelperTriggerReg = /[\w\d?_()[\]'"`]/
+        if (!editorHelperTriggerReg.test(prevChar)) return
+
+        return {
+          suggestions: [
+            {
+              label: 'par',
+              detail: '(expr)'
+            },
+            {
+              label: 'var',
+              detail: 'var name = expr'
+            }
+          ]
+        }
+      }
+    }))
+    return {
+      dispose() {
+        dependDisposable.dispose()
+      }
+    }
+  }
+})
