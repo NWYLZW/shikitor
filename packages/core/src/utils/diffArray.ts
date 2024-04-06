5c00ba2705e85669d42fd7b6f05256259fddb618
yijie4188 <yijie4188@gmail.com>
2024-04-06 19:12:02 +0800

    feat(core): add diff array util function

---
+++ packages/core/src/utils/diffArray.ts
@@ -0,0 +1,41 @@
+interface Diff<T> {
+  added: T[]
+  removed: T[]
+  reordered: [oldIndex: number, newIndex: number][]
+}
+
+export function diffArray<T>(base: T[], newA: T[], isEqual: (a: T, b: T) => boolean = (a, b) => a === b): Diff<T> {
+  const diff: Diff<T> = {
+    added: [],
+    removed: [],
+    reordered: []
+  }
+  if (base === newA) return diff
+  if (base.length === 0) {
+    diff.added = newA
+    return diff
+  }
+  if (newA.length === 0) {
+    diff.removed = base
+    return diff
+  }
+
+  for (let i = 0; i < base.length; i++) {
+    const item = base[i]
+    const index = newA.findIndex(newItem => isEqual(item, newItem))
+    if (index === -1) {
+      diff.removed.push(item)
+    } else if (index !== i) {
+      diff.reordered.push([i, index])
+    }
+  }
+
+  for (let i = 0; i < newA.length; i++) {
+    const item = newA[i]
+    const index = base.findIndex(baseItem => isEqual(item, baseItem))
+    if (index === -1) {
+      diff.added.push(item)
+    }
+  }
+  return diff
+}
