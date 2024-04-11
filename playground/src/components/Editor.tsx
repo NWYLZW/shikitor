16bd494a8e4798c69226e26c5af3e4f5661f0a69
yijie4188 <yijie4188@gmail.com>
2024-04-11 12:49:41 +0800

    feat(play): support mount event and ref prop

--- playground/src/components/Editor.tsx
+++ playground/src/components/Editor.tsx
@@ -1,6 +1,6 @@
 import type { Shikitor, ShikitorOptions } from '@shikitor/core'
 import type { create } from '@shikitor/core'
-import React, { useEffect, useRef } from 'react'
+import React, { forwardRef, useCallback, useEffect, useRef } from 'react'
 
 import type { GistFile } from '../utils/gist'
 import { getGist } from '../utils/gist'
@@ -13,14 +13,29 @@ export interface EditorProps {
    * @internal
    */
   create?: typeof create
+  onMounted?(shikitor: Shikitor): void
 }
 
-export default function Editor(props: EditorProps) {
+export type EditorRef = Partial<Shikitor>
+
+export default forwardRef<EditorRef, EditorProps>(function Editor(props, ref) {
   const {
     options,
     defaultOptions,
-    create
+    create,
+    onMounted
   } = props
+  const mount = useCallback((shikitor: Shikitor) => {
+    shikitorRef.current = shikitor
+    if (ref) {
+      if (typeof ref === 'function') {
+        ref(shikitor)
+      } else {
+        ref.current = shikitor
+      }
+    }
+    onMounted?.(shikitor)
+  }, [onMounted, ref])
 
   const defaultOptionsRef = useRef(defaultOptions)
   const initialOptionsRef = useRef(Object.assign({}, defaultOptions, options))
@@ -48,65 +63,7 @@ export default function Editor(props: EditorProps) {
     const abortController = new AbortController()
     const abortSignal = abortController.signal
     create?.(ele, initialOptionsRef.current, { abort: abortSignal })
-      .then(s => {
-        shikitorRef.current = s
-        console.log('shikitor created')
-        // shikitor.focus(config.cursor?.offset)
-        // await shikitor.upsertPlugin({
-        //   name: 'shikitor-saver',
-        //   onKeydown(e) {
-        //     if (e.key === 's' && (e.ctrlKey || e.metaKey)) {
-        //       e.preventDefault()
-        //       const code = this.value
-        //       const url = new URL(location.href)
-        //
-        //       let newHashStr = ''
-        //       if (code !== DEFAULT_CODE) {
-        //         newHashStr = `zip-code/${zipStr(code)}`
-        //       }
-        //       url.hash = newHashStr
-        //
-        //       const query = new URLSearchParams()
-        //       this.options.language
-        //       && query.set('language', this.options.language)
-        //       this.options.theme
-        //       && query.set('theme', this.options.theme)
-        //       query.set('fullscreen', String(fullscreenCount))
-        //       url.search = query.toString()
-        //       history.pushState(null, '', url.toString())
-        //     }
-        //   }
-        // })
-        // if (hashType === 'gist') {
-        //   shikitor.value = '// Loading from gist...'
-        //   const [hash, filename, revision] = hashContent.split('/')
-        //   let isNotFound = false
-        //   let files: GistFile[] | undefined
-        //   try {
-        //     const { file, files: getFiles } = await getGist(hash, filename, revision)
-        //     if (file?.content) {
-        //       shikitor.value = file?.content
-        //     } else {
-        //       isNotFound = true
-        //       files = Object.values(getFiles)
-        //     }
-        //   } catch (e) {
-        //     const error = e as Error
-        //     if ('message' in error && error.message === 'Not Found') {
-        //       isNotFound = true
-        //     } else {
-        //       throw e
-        //     }
-        //   }
-        //   if (isNotFound) {
-        //     if (files) {
-        //       console.error('File not found, available files are: ' + files.map(file => file.filename).join(', '))
-        //     } else {
-        //       console.error('Gist not found, the hash may be invalid or the gist is private')
-        //     }
-        //   }
-        // }
-      })
+      .then(mount)
       .catch(e => {
         if (e instanceof Error && e.message === 'Aborted') return
         console.error(e)
@@ -115,7 +72,7 @@ export default function Editor(props: EditorProps) {
       abortController.abort()
       shikitorRef.current?.[Symbol.dispose]()
     }
-  }, [create])
+  }, [create, mount])
   useEffect(() => {
     const shikitor = shikitorRef.current
     if (!shikitor) return
@@ -123,4 +80,4 @@ export default function Editor(props: EditorProps) {
     options && shikitor.updateOptions(options)
   }, [options])
   return <div ref={eleRef} />
-}
+})
