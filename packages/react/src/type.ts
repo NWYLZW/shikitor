b029e74d16982f60b08e0755f212e5272bf6a6ea
yijie4188 <yijie4188@gmail.com>
2024-04-17 22:58:21 +0800

    feat(react): support listen value change

--- packages/react/src/type.ts
+++ packages/react/src/type.ts
@@ -3,6 +3,9 @@ import type { Shikitor, ShikitorOptions } from '@shikitor/core'
 export interface EditorProps {
   options?: ShikitorOptions
   defaultOptions?: ShikitorOptions
+  value?: string
+  defaultValue?: string
+  onChange?(value: string): void
   onMounted?(shikitor: Shikitor): void
   onColorChange?(color: {
     bg: string
