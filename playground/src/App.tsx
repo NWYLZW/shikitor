34cd3a78dbed80056e1fc6f434df2b303c20bf6d
yijie4188 <yijie4188@gmail.com>
2024-04-23 04:33:38 +0800

    feat(play): example list

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -69,32 +69,78 @@ export default function App() {
   const shikitorCreate = useShikitorCreate()
   usePlugins(shikitorRef)
   return (
-    <div className='card'>
-      <CardHeader />
-      <MemoEditor
-        ref={shikitorRef}
-        create={shikitorCreate}
-        value={code}
-        onChange={setCode}
-        options={useMemo(() => ({
-          theme,
-          language
-        }), [theme, language])}
-        plugins={plugins}
-        onColorChange={({ bg, fg }) => {
-          const style = document.documentElement.style
-          style.setProperty('--bg', bg)
-          style.setProperty('--td-font-gray-1', bg)
-          style.setProperty('--td-text-color-anti', bg)
-          style.setProperty('--fg', fg)
-          style.setProperty('--td-bg-color-container', fg)
-          style.setProperty('--td-gray-color-13', fg)
-          const hoverColor = `color-mix(in srgb, ${fg}, ${bg} 10%)`
-          style.setProperty('--hover', hoverColor)
-          style.setProperty('--td-gray-color-1', hoverColor)
-        }}
-        onMounted={initPlaygroundShikitor}
-      />
-    </div>
+    <>
+      <div className='card'>
+        <CardHeader />
+        <MemoEditor
+          ref={shikitorRef}
+          create={shikitorCreate}
+          value={code}
+          onChange={setCode}
+          options={useMemo(() => ({
+            theme,
+            language
+          }), [theme, language])}
+          plugins={plugins}
+          onColorChange={({ bg, fg }) => {
+            const style = document.documentElement.style
+            style.setProperty('--bg', bg)
+            style.setProperty('--td-font-gray-1', bg)
+            style.setProperty('--td-text-color-anti', bg)
+            style.setProperty('--fg', fg)
+            style.setProperty('--td-bg-color-container', fg)
+            style.setProperty('--td-gray-color-13', fg)
+            const hoverColor = `color-mix(in srgb, ${fg}, ${bg} 10%)`
+            style.setProperty('--hover', hoverColor)
+            style.setProperty('--td-gray-color-1', hoverColor)
+          }}
+          onMounted={initPlaygroundShikitor}
+        />
+      </div>
+      <div className='examples'>
+        <div className='example active'>
+          <h4>Code Editor</h4>
+          <div className='preview'>
+            <div className='code-block-line'>
+              <pre className='code-block' style={{ backgroundColor: '#bd89e2' }}>{' '.repeat(2)}</pre>
+              <pre className='code-block' style={{ backgroundColor: '#e87d85' }}>{' '.repeat(5)}</pre>
+              <pre className='code-block' style={{ backgroundColor: '#9faebe' }}>{' '}</pre>
+            </div>
+            <div className='code-block-line'>
+              <pre className='code-block' style={{ backgroundColor: 'transparent' }}>{' '.repeat(2)}</pre>
+              <pre className='code-block' style={{ backgroundColor: '#8ad097' }}>{' '.repeat(3)}</pre>
+            </div>
+            <div className='code-block-line highlight'>
+              <pre className='code-block' style={{ backgroundColor: 'transparent' }}>{' '.repeat(2)}</pre>
+              <pre className='code-block' style={{ backgroundColor: '#8ad097' }}>{' '.repeat(6)}</pre>
+              <pre className='code-block' style={{ backgroundColor: '#e2b876' }}>{' '.repeat(2)}</pre>
+              <div className='code-cursor' />
+              <div className='code-completions'>
+                <div className='code-completion'></div>
+                <div className='code-completion'></div>
+              </div>
+            </div>
+            <div className='code-block-line'>
+              <pre className='code-block' style={{ backgroundColor: '#9faebe' }}>{' '}</pre>
+            </div>
+            <div className='code-block-line'>
+              <pre className='code-block' style={{ backgroundColor: '#e87d85' }}>{' '.repeat(5)}</pre>
+            </div>
+          </div>
+        </div>
+        <div className='example'>
+          <h4>Markdown Editor</h4>
+          <div className='preview'>
+            Not implemented
+          </div>
+        </div>
+        <div className='example'>
+          <h4>Message Sender</h4>
+          <div className='preview'>
+            Not implemented
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
