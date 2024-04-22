3f9be4902f8b9d8b08f9240c5196a8a0df1ff98c
yijie4188 <yijie4188@gmail.com>
2024-04-23 02:19:34 +0800

    refactor(play): abstract CardHeader style

--- playground/src/components/CardHeader.tsx
+++ playground/src/components/CardHeader.tsx
@@ -1,3 +1,5 @@
+import './CardHeader.scss'
+
 import React from 'react'
 import { type BundledLanguage, bundledLanguagesInfo, type BundledTheme, bundledThemesInfo } from 'shiki'
 import { Fullscreen1Icon, FullscreenExit1Icon } from 'tdesign-icons-react'
@@ -16,55 +18,60 @@ export function CardHeader() {
     language = 'typescript',
     viewMode = 'normal'
   } = queries.value ?? {}
-  return <div className='header'>
-    <div className='left'>
-      <Select
-        filterable
-        borderless
-        size='small'
-        value={language}
-        onChange={v => queries.set('language', v as string)}
-        options={bundledLanguagesInfo.map(lang => ({
-          value: lang.id,
-          label: lang.name
-        }))}
-      />
-      <Select
-        filterable
-        borderless
-        size='small'
-        value={theme}
-        onChange={v => queries.set('theme', v as string)}
-        options={bundledThemesInfo.map(theme => ({
-          value: theme.id,
-          label: theme.displayName
-        }))}
-      />
+  return (
+    <div className='card-header'>
+      <div className='left'>
+        <Select
+          filterable
+          borderless
+          size='small'
+          value={language}
+          onChange={v => queries.set('language', v as string)}
+          options={bundledLanguagesInfo.map(lang => ({
+            value: lang.id,
+            label: lang.name
+          }))}
+        />
+        <Select
+          filterable
+          borderless
+          size='small'
+          value={theme}
+          onChange={v => queries.set('theme', v as string)}
+          options={bundledThemesInfo.map(theme => ({
+            value: theme.id,
+            label: theme.displayName
+          }))}
+        />
+      </div>
+      <div className='right'>
+        <Button
+          ghost
+          variant='text'
+          shape='square'
+          icon={{
+            normal: <Fullscreen1Icon />,
+            'fullscreen-page': <Fullscreen1Icon />,
+            'fullscreen-screen': <FullscreenExit1Icon />
+          }[viewMode]}
+          onClick={() =>
+            queries.set(
+              'viewMode',
+              viewMode === 'normal'
+                ? 'fullscreen-page'
+                : viewMode === 'fullscreen-page'
+                ? 'fullscreen-screen'
+                : 'normal'
+            )}
+        />
+        <Link
+          hover='color'
+          href='https://github.com/nwylzw/shikitor'
+          target='_blank'
+        >
+          <img src='https://github.githubassets.com/favicons/favicon.svg' alt='github' width='16' height='16' />
+        </Link>
+      </div>
     </div>
-    <div className='right'>
-      <Button
-        ghost
-        variant='text'
-        shape='square'
-        icon={{
-          normal: <Fullscreen1Icon/>,
-          'fullscreen-page': <Fullscreen1Icon/>,
-          'fullscreen-screen': <FullscreenExit1Icon/>
-        }[viewMode]}
-        onClick={() => queries.set('viewMode', viewMode === 'normal'
-          ? 'fullscreen-page'
-          : viewMode === 'fullscreen-page'
-            ? 'fullscreen-screen'
-            : 'normal'
-        )}
-      />
-      <Link
-        hover='color'
-        href='https://github.com/nwylzw/shikitor'
-        target='_blank'
-      >
-        <img src='https://github.githubassets.com/favicons/favicon.svg' alt='github' width='16' height='16'/>
-      </Link>
-    </div>
-  </div>
+  )
 }
