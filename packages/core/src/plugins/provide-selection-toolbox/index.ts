37d9674d5170a3a1b84fa0418a3eff66121dc4b6
yijie4188 <yijie4188@gmail.com>
2024-05-07 00:49:22 +0800

    feat(plugin-provide-toolbox): implement register tools logic structure

--- packages/core/src/plugins/provide-selection-toolbox/index.ts
+++ packages/core/src/plugins/provide-selection-toolbox/index.ts
@@ -2,17 +2,42 @@ import './index.scss'
 
 import type { IDisposable, LanguageSelector, ProviderResult } from '@shikitor/core'
 import { derive } from 'valtio/utils'
+import { proxy, ref, snapshot } from 'valtio/vanilla'
 
 import { definePlugin } from '../../plugin'
+import type { Nullable } from '../../types'
+import { icon, isUnset, UNSET } from '../../utils' with { 'unbundled-reexport': 'on' }
 import { scoped } from '../../utils/valtio/scoped'
 
 const name = 'provide-selection-toolbox'
 
-export interface ToolInner {
-  label?: string
-  title?: string
-  type?: 'button' | 'toggle' | 'select'
+export type ToolInner =
+  & {
+    title?: string
+  }
+  & (
+    | {
+      label?: string
+      icon?: string
+      type?: 'button'
+    }
+    | {
+      label?: string
+      type?: 'toggle' | 'select'
+    }
+  )
+
+function toolItemTemplate(tool: ToolInner) {
+  const { prefix } = toolItemTemplate
+  if (tool.type === 'button') {
+    return `<div class='${prefix}'>
+      ${tool.icon ? icon(tool.icon ?? '', `${prefix}__btn`) : ''}
+      <div class='${prefix}__label'>${tool.label}</div>
+    </div>`
+  }
+  return ''
 }
+toolItemTemplate.prefix = `${'shikitor'}-tool-item`
 
 declare module '@shikitor/core' {
   export type Tool = ToolInner
@@ -41,12 +66,32 @@ export default () =>
           current: get => get(optionsRef).current.language
         })
         const { disposeScoped, scopeWatch } = scoped()
-        const tools = []
+        const elementRef = proxy({ current: ref<HTMLDivElement | typeof UNSET>(UNSET) })
+        const tools = proxy<ToolInner[]>([])
+        scopeWatch(get => {
+          const toolsSnapshot = snapshot(get(tools))
+          const element = elementRef.current
+          if (isUnset(element)) return
+          if (toolsSnapshot.length === 0) {
+            element.style.visibility = 'hidden'
+            element.innerHTML = ''
+            return
+          }
+          element.style.visibility = 'visible'
+          element.innerHTML = toolsSnapshot.map(toolItemTemplate).join('')
+        })
         const disposeSelectionToolsExtend = shikitor.extend('provide-selection-toolbox', {
           registerSelectionToolsProvider(selector, provider) {
-            const disposeWatcher = scopeWatch(get => {
+            let providerDispose: Nullable<() => void>
+            const { provideSelectionTools } = provider
+            const disposeWatcher = scopeWatch(async get => {
               const language = get(languageRef).current
               if (selector !== '*' && selector !== language) return
+              providerDispose?.()
+              const [{ start, end }] = shikitor.selections
+              const selection = shikitor.value.slice(start.offset, end.offset)
+              const { tools: newTools, dispose } = await provideSelectionTools(selection) ?? {}
+              providerDispose = dispose
             })
             return {
               dispose: () => disposeWatcher()
@@ -63,15 +108,8 @@ export default () =>
             popups: [{
               id: 'selection-toolbox',
               render(ele) {
-                if (shikitor.language !== 'markdown') return
-                ele.innerHTML = `
-                  <div class='btn blob'>B</div>
-                  <div class='btn italic'>I</div>
-                  <div class='btn underline'>U</div>
-                  <div class='btn strikethrough'>S</div>
-                  <div class='btn code'>&lt;&gt;</div>
-                  <div class='btn link'>a</div>
-                `
+                ele.style.visibility = 'hidden'
+                elementRef.current = ref(ele)
               }
             }]
           })
@@ -80,6 +118,7 @@ export default () =>
         return {
           dispose() {
             disposeSelectionToolsExtend?.()
+            console.log(disposeSelectionToolboxProvider)
             disposeSelectionToolboxProvider?.()
             disposeScoped()
           }
