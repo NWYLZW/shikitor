d0819d82ba426e4884d8f7edd6c0df36573038c3
Olnyhhhh <55044991+Only566@users.noreply.github.com>
2024-05-08 00:00:55 +0800

    feat(plugin-symmetry-operator): add symmetry operator plugin (#9)

---
+++ packages/core/src/plugins/symmetry-operator.ts
@@ -0,0 +1,32 @@
+import { definePlugin } from '@shikitor/core'
+
+const symmetryOperatorMapping: Record<string, string> = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
+  '`': '`',
+  '\'': '\'',
+  '"': '"'
+}
+function isSymmetryOperatorKey(key: string) {
+  return key in symmetryOperatorMapping
+}
+const name = 'symmetry-operator'
+export default () => {
+  return definePlugin({
+    name,
+    onKeydown(e) {
+      const textarea = e.target
+      const [{ start, end }] = this.selections ?? [{}]
+      if (start.offset === end.offset) return
+      if (isSymmetryOperatorKey(e.key) && !(e.metaKey || e.ctrlKey)) {
+        textarea.setRangeText(symmetryOperatorMapping[e.key], end.offset, end.offset)
+        textarea.setRangeText(e.key, start.offset, start.offset)
+        textarea.dispatchEvent(new Event('input'))
+        this.updateSelection(0, { start: end.offset + 2, end: end.offset + 2 })
+        e.preventDefault()
+      }
+    }
+  })
+}
