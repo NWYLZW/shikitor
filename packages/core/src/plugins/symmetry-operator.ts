24b4de04e795a81e2cac10217945d6504a724766
LGH <sunshine4188y@gmail.com>
2024-05-07 23:25:47 +0800

    refactor(symmetry-operator): rename prevBracketMapping to symmetryOperatorMapping

--- packages/core/src/plugins/symmetry-operator.ts
+++ packages/core/src/plugins/symmetry-operator.ts
@@ -1,25 +1,27 @@
 import { definePlugin } from '@shikitor/core'
 
-const prevBracketMapping: Record<string, string> = {
+const symmetryOperatorMapping: Record<string, string> = {
   '(': ')',
   '[': ']',
   '{': '}',
-  '<': '>'
+  '<': '>',
+  '`': '`',
+  '\'': '\'',
+  '"': '"'
 }
-function isPrevBracketKey(key: string) {
-  return key in prevBracketMapping
+function isSymmetryOperatorKey(key: string) {
+  return key in symmetryOperatorMapping
 }
 const name = 'symmetry-operator'
 export default () => {
   return definePlugin({
     name,
     onKeydown(e) {
-      console.log(e)
       const textarea = e.target
       const [{ start, end }] = this.selections
       if (start.offset === end.offset) return
-      if (isPrevBracketKey(e.key) && !(e.metaKey || e.ctrlKey)) {
-        textarea.setRangeText(prevBracketMapping[e.key], end.offset, end.offset)
+      if (isSymmetryOperatorKey(e.key) && !(e.metaKey || e.ctrlKey)) {
+        textarea.setRangeText(symmetryOperatorMapping[e.key], end.offset, end.offset)
         textarea.setRangeText(e.key, start.offset, start.offset)
         textarea.dispatchEvent(new Event('input'))
         this.updateSelection(0, { start: end.offset + 2, end: end.offset + 2 })
