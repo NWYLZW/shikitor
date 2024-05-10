90085d0aad53e35d7b5d9dfc431c35a4e3ca78c2
yijie4188 <yijie4188@gmail.com>
2024-05-10 18:34:18 +0800

    style(play): dprint

--- playground/src/hooks/usePlugins.ts
+++ playground/src/hooks/usePlugins.ts
@@ -5,17 +5,17 @@ import { useEffect } from 'react'
 import { onPluginChange } from '../plugins'
 
 export const usePlugins = (shikitorRef: RefObject<Shikitor>) => {
-  useEffect(() => onPluginChange((index, id, module) => {
-    const shikitor = shikitorRef.current
-    if (!shikitor) return
+  useEffect(() =>
+    onPluginChange((index, id, module) => {
+      const shikitor = shikitorRef.current
+      if (!shikitor) return
 
-    const findIndex = shikitor.options.plugins.findIndex(p => p.name === id)
-    shikitor
-      .upsertPlugin(module, findIndex === -1 ? undefined : findIndex)
-      .then(() => {
-        console.log('Plugin updated:', id)
-      })
-  }), [shikitorRef])
-  return {
-  }
+      const findIndex = shikitor.options.plugins.findIndex(p => p.name === id)
+      shikitor
+        .upsertPlugin(module, findIndex === -1 ? undefined : findIndex)
+        .then(() => {
+          console.log('Plugin updated:', id)
+        })
+    }), [shikitorRef])
+  return {}
 }
