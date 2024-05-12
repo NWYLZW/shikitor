734cd9e80e17449eb7bae4a062cd380cfb5e5f83
yijie4188 <yijie4188@gmail.com>
2024-05-12 16:11:02 +0800

    feat(plugin-provide-selection-toolbox): alignment active and hover color

--- packages/core/src/creator/structureTransfomer.ts
+++ packages/core/src/creator/structureTransfomer.ts
@@ -16,6 +16,14 @@ export function shikitorStructureTransformer(
       const fg = div.style.color
       target.style.setProperty(cssvar('fg-color'), fg)
       target.style.setProperty(cssvar('bg-color'), bg)
+      target.style.setProperty(
+        cssvar('hv-color'),
+        `color-mix(in srgb, var(${cssvar('bg-color')}), var(${cssvar('fg-color')}) 90%)`
+      )
+      target.style.setProperty(
+        cssvar('active-color'),
+        `color-mix(in srgb, var(${cssvar('bg-color')}), var(${cssvar('fg-color')}) 70%)`
+      )
       target.style.setProperty(cssvar('caret-color'), fg)
       target.style.color = fg
       target.style.backgroundColor = bg
