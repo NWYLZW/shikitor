bf0fe3c2d97807df436efa93835449ef19cf57c7
yijie4188 <yijie4188@gmail.com>
2024-05-16 02:25:55 +0800

    fix(react): resolve empty string is falsy, and can't assign variable

--- packages/react/src/hooks/useDefault.ts
+++ packages/react/src/hooks/useDefault.ts
@@ -7,18 +7,22 @@ export function useDefault<T>(
 ) {
   const defaultRef = useRef(defaultV)
   const isObject = useMemo(() => typeof defaultRef.current === 'object', [])
-  const vRef = useRef(isObject
-    ? Object.assign({}, defaultV, v)
-    : v)
+  const vRef = useRef(
+    isObject
+      ? Object.assign({}, defaultV, v)
+      : v
+  )
   useEffect(() => {
     vRef.current = isObject
       ? Object.assign({}, defaultRef.current, v)
       : v
   }, [isObject, v])
   useEffect(() => {
-    if (!vRef.current) return
+    if (vRef.current === void 0 || vRef.current === null) return
 
-    v && onChange?.(vRef.current)
+    if (v !== void 0 && v !== null) {
+      onChange?.(vRef.current)
+    }
   }, [onChange, v])
 
   return { vRef }
